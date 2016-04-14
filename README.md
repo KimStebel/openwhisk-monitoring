1387abcb6e9ecad5969c40c3f477656668c9264f
Kim Stebel <kim.stebel@gmail.com>
2016-04-14 15:53:59 +0100

    readme, comments

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-# Basic monitoring with openwhisk
+# Basic server monitoring with OpenWhisk
 
+I've created this example to monitor the status of a [tmpnb](https://github.com/jupyter/tmpnb) server I'm running, but it can easily be modified to monitor any HTTP server.
 
+## Deploy
 
+ * Sign up for [OpenWhisk](http://www.ibm.com/cloud-computing/bluemix/openwhisk/).
+ * [Install the cli.](https://console.ng.bluemix.net/openwhisk/cli)
+ * Sign up for a postmark account (or use a different email service and change the mailing code in monitor.js).
+ * Place the postmark token into a file named `secret` with the following content:
+```
+POSTMARK_TOKEN='....'
+```
+ * run `./setup`
+ 
+## Known issues
+
+Currently each trigger can be executed a maximum of 10000 times. If you execute a trigger every 10 minutes, that will only last a bit over 2 months. After that, a new trigger will have to be created. 
--- monitor.js
+++ monitor.js
@@ -1,23 +1,23 @@
-var request = require('request');
+var request = require('request'); // request library for making http requests
 
 function main() {
-	var url = 'https://tmpnb.kimstebel.com:8000/api/stats';
+	var url = 'https://tmpnb.kimstebel.com:8000/api/stats'; // stats endpoint telling you how many docker containers are available in tmpnb server
   request.get(url, function(error, response, body) {
-    var available = JSON.parse(body).available;
-    var options = {
+    var available = JSON.parse(body).available; // number of available docker containers
+    var options = { // http request to send an email via postmark
       url: 'https://api.postmarkapp.com/email',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        'X-Postmark-Server-Token': 'AAAPOSTMARKTOKENAAA'
+        'X-Postmark-Server-Token': 'AAAPOSTMARKTOKENAAA' //auth token gets inserted here
       },
       json: true,
       body: { From: 'mail@kimstebel.com', To: 'kim.stebel@gmail.com', Subject: 'Postmark test', HtmlBody: '<html><body>Only ' + available + ' available.</body></html>' }
     };
-    if (available < 20) {
-      request.post(options, function(error, response, body) {});
+    if (available < 20) { //not enough containers available?
+      request.post(options, function(error, response, body) {}); //email me
     }
-    whisk.done({available: available});
+    whisk.done({available: available}); // include number of available containers in action result
   });
 
   return whisk.async();
